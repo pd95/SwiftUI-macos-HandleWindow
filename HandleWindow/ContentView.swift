c26374786257f13c63a1317cfab2d2c991cbd614
Philipp <pd95@users.noreply.github.com>
2022-06-18 16:45:01 +0200

    Initial Commit

---
+++ HandleWindow/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  HandleWindow
+//
+//  Created by Philipp on 18.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HandleWindow/HandleWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandleWindowApp.swift
+//  HandleWindow
+//
+//  Created by Philipp on 18.06.22.
+//
+
+import SwiftUI
+
+@main
+struct HandleWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
