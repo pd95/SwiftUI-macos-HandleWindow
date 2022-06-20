edc1b0136e277eb8e048572f1ab5b0542d2b26b1
Philipp <pd95@users.noreply.github.com>
2022-06-20 13:38:25 +0200

    Add settings window with "Reset & Restart"

--- HandleWindow/HandleWindowApp.swift
+++ HandleWindow/HandleWindowApp.swift
@@ -14,5 +14,30 @@ struct HandleWindowApp: App {
         WindowGroup(id: "main") {
             ContentWindowWrapper()
         }
+
+        Settings {
+            GroupBox {
+                Button("Reset to defaults & restart", action: resetToDefaults)
+                    .padding()
+            }
+            .padding()
+        }
+    }
+
+    private func resetToDefaults() {
+        let defaults = UserDefaults.standard
+
+        let identifier = Bundle.main.bundleIdentifier!
+        defaults.removePersistentDomain(forName: identifier)
+        defaults.synchronize()
+
+
+        let url = URL(fileURLWithPath: Bundle.main.resourcePath!)
+        let path = url.deletingLastPathComponent().deletingLastPathComponent().absoluteString
+        let task = Process()
+        task.launchPath = "/usr/bin/open"
+        task.arguments = [path]
+        task.launch()
+        NSApplication.shared.stop(nil)
     }
 }
